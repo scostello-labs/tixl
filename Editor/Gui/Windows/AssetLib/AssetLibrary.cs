62bd409a4085786c2b677919b6d3f2f6f4bfe6b3
Thomas Mann <thomas.mann@framefield.com>
2026-01-14 22:28:17 +0100

    Files can be dragged directly onto the canvas

--- Editor/Gui/Windows/AssetLib/AssetLibrary.cs
+++ Editor/Gui/Windows/AssetLib/AssetLibrary.cs
@@ -86,7 +86,7 @@ internal sealed partial class AssetLibrary : Window
 
                 var fileInfo = new FileInfo(absolutePath);
                 var fileInfoExtension = fileInfo.Extension.Length < 1 ? string.Empty : fileInfo.Extension[1..];
-                var fileExtensionId = FileExtensionRegistry.GetId(fileInfoExtension);
+                var fileExtensionId = FileExtensionRegistry.GetUniqueId(fileInfoExtension);
                 if (!AssetTypeRegistry.TryGetFromId(fileExtensionId, out var assetType))
                 {
                     Log.Warning($"Can't find file type for: {fileInfoExtension}");
